635f5023fa6b74388809de2eb0cda808c4603d67
Chronic Coder <efeoghene.obebeduo@gmail.com>
2024-05-02 01:34:20 -0700

    Fix flake8 errors

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=["lxml"],
-    entry_points = {
-        'console_scripts': ['ssm=ssm.ssm:main' ]
+    entry_points={
+        "console_scripts": ["ssm=ssm.ssm:main"]
     },
 )

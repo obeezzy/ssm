f9936914011bb64014a478b7ca90c59a27536d35
Chronic Coder <efeoghene.obebeduo@gmail.com>
2024-05-02 11:52:24 -0700

    Bump up version to v0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (ROOT_DIR / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ssm-svg",
-    version="0.0.2",
+    version="0.0.3",
     description="SVG spritesheet maker",
     long_description=README,
     long_description_content_type="text/markdown",

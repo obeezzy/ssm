c4f0c520d1cbe268d67983513420a5ddf612dd56
Chronic Coder <efeoghene.obebeduo@gmail.com>
2024-05-02 20:56:18 -0700

    Update version to v0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (ROOT_DIR / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ssm-svg",
-    version="0.0.5",
+    version="0.0.6",
     description="SVG spritesheet maker",
     long_description=README,
     long_description_content_type="text/markdown",
